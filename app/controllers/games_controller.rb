315baa6f02c1f0fe8e2c4ba70575c9035ff1b775
Paul Fouquet <Skypol59@msn.com>
2025-04-01 10:16:42 +0200

    restored

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,13 +2,21 @@ require "json"
 require "open-uri"
 
 class GamesController < ApplicationController
+  # VOWELS = %w(A E I O U Y)
+
 
   def new
     @letters = []
     10.times do
-      @letters << ('a'..'z').to_a.sample
-    end
+     @letters << ('a'..'z').to_a.sample
+   end
   end
+
+  # def new
+  #   @letters = Array.new(5) { VOWELS.sample }
+  #   @letters += Array.new(5) { (('A'..'Z').to_a - VOWELS).sample }
+  #   @letters.shuffle!
+  # end
 
   def score
     url = "https://dictionary.lewagon.com/#{params[:attempt]}" # chercher le mot(params[:attempt]) dans le dico
